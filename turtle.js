ad9bfca862a63150593892fd947d5a02b83c3b2f
Dice1K <62322488+Dice1K@users.noreply.github.com>
2023-06-12 22:51:23 +0900

    made a circle and removed redudant lines

---
+++ turtle.js
@@ -0,0 +1,13 @@
+setSpeed(1);
+penup();
+left(90);
+pendown();
+function draw_circle(x){
+    for (i = 0; i <= x; i ++){
+        forward(10);
+        right(5);
+    }
+    return x;
+}
+
+let circle = draw_circle(70);
