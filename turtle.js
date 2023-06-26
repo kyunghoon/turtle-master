80cecf677c841e198e78cd277ebf6e6970430cb4
Daisuke Watanabe <dais@Daisukes-MacBook-Air.local>
2023-06-26 10:15:59 +0900

    merged turtle.js

--- turtle.js
+++ turtle.js
@@ -1,20 +1,33 @@
-setSpeed(1);
-penup();
-left(90);
-pendown();
-function draw_circle(x){
-    for (i = 0; i <= x; i++){
-        forward(10);
-        right(5);
+setSpeed(50);
+goto(-160,0);
+
+function draw_circle(distance, angle, sides){
+    for(i = 0; i < sides; i++){
+        forward(distance);
+        right(angle);
+        
     }
-    return x;
+    pieces = 360/sides;
+
+        // create variable that gives you the part of the circle
+        // the above is close, need for loop and a variable.
+        //forward(20);
+        // always know what the function takes and what it returns.
+
+
+        //how to make the angle a variable
+        // how to make the diretion a variable. 
+        // both diviisible by the the circle degree 360.
+
+
+    return pieces;
 }
 
-let circle = draw_circle(70);
 
+let circle = draw_circle(15, 5, 72);
 
-/// make 3 triangle
-/// 4 square
-/// etc
+// console.log(circle);
+//HINT circle = 360 
+//IF I divide a pie by 3 divide 360 by 3
 
- 
+// make the "n" in draw_circle(n) the number of sides you need.
